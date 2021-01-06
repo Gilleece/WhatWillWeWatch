cc77c5b5cf2847627a845a4ad1a59ca1528e8f7a
Gilleece <seangilleece@gmail.com>
2021-01-06 22:57:03 +0000

    Genre selectors now dynamically generated on load from a single block of html, second and third selects are disabled by default

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,40 @@
 HTML Generation and user input handling. 
 */
 
+// This function generates the HTML for the genre selections.
+
+function generateGenreHtml() {
+    for (i = 1; i < 4; i++) {
+        $("#genreSelector").append(`
+        <select id="genre${i}" name="genre${i}">
+            <option value="none" selected="selected">-</option>
+            <option value="28">Action</option>
+            <option value="12">Adventure</option>
+            <option value="16">Animation</option>
+            <option value="35">Comedy</option>
+            <option value="80">Crime</option>
+            <option value="99">Documentary</option>
+            <option value="18">Drama</option>
+            <option value="10751">Family</option>
+            <option value="14">Fantasy</option>
+            <option value="36">History</option>
+            <option value="27">Horror</option>
+            <option value="10402">Music</option>
+            <option value="9648">Mystery</option>
+            <option value="10749">Romance</option>
+            <option value="878">Science Fiction</option>
+            <option value="10770">TV Movie</option>
+            <option value="53">Thriller</option>
+            <option value="10752">War</option>
+            <option value="37">Western</option>
+        </select>
+        `);
+    }; 
+    // Makes second and third genre selector disabled on load
+    $("#genre2").attr("disabled", true); 
+    $("#genre3").attr("disabled", true);
+}
+
 // This function contains, and generates, the HTML for each recommendation card.
 function generateCardHtml(i) {
     $(`#recommendationRow`).append(` 
