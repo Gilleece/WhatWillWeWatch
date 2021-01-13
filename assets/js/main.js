0e675be1e4aa6514b3c5c908000b31bc8c33e409
Gilleece <seangilleece@gmail.com>
2021-01-13 15:30:49 +0000

    Updated the wording of text on line 107

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,7 +104,7 @@ function generateCardHtml(i) {
                                     <h5 class="mb-0">
                                         <button class="btn btn-primary more-details-button" data-toggle="collapse" data-target="#collapse${i}"
                                             aria-expanded="true" aria-controls="collapse${i}">
-                                            Click for details <i class="fas fa-info-circle"></i>
+                                            Click here for details <i class="fas fa-info-circle"></i>
                                         </button>
                                     </h5>
                                 </div>
